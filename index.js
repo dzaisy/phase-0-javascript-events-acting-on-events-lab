e4c756ca5506a9645e1cd81f71d8ef01238c9caf
kudzaishe muzata <kudzaishemmuzata@gmail.com>
2024-03-14 22:36:22 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+// grabs the dodger
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  // build our moveDodgerLeft() function, adding a check on the current position of the dodger:
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+ //Now let's wire this up to our event listener 
+
+ function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+ }
+
+ document.addEventListener("keydown", function (e) {
+    if (e.key == "ArrowLeft") {
+        moveDodgerRight();
+    }
+ });
+
